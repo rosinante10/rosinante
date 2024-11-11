318ac3f25d29518fe45a251d3e420237c9289f1d
rosinante10 <stopedgethelp@gmail.com>
2024-11-12 00:42:14 +0300

    Update Поиск обощих участников.py

--- Поиск обощих участников.py
+++ Поиск обощих участников.py
@@ -4,16 +4,19 @@
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-# TODO Провеьте работу функции с разделителем отличным от запятой
-def find_common_participants(s1, s2, sept = ','):
-    sarr1 = s1.split(sept)
-    sarr2 = s2.split(sept)
+
+def find_common_participants(s1, s2, sep = ','):
+    sarr1 = s1.split(sep)
+    sarr2 = s2.split(sep)
     all = []
     for i in sarr1:
         for j in sarr2:
-            print(i, j)
             if i == j:
                 all.append(i)
     all.sort()
     return all
-print(find_common_participants(participants_first_group, participants_second_group, "|"))
+
+
+
+a = find_common_participants(participants_first_group, participants_second_group,"|")
+print("Общие участники:", a)
