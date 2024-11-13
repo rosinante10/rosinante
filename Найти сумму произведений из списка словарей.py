fc5a2b646c7d369fc3b92b6e4f3e8e09c1751726
rosinante10 <stopedgethelp@gmail.com>
2024-11-13 11:20:58 +0300

    Update Найти сумму произведений из списка словарей.py

--- Найти сумму произведений из списка словарей.py
+++ Найти сумму произведений из списка словарей.py
@@ -1,14 +1,13 @@
-# TODO решите задачу
-import json
-def task() -> float:
-    f = open("input.json")
-    data = json.load(f)
-    sum = 0
-    for i in data:
-        n = i["score"]*i["weight"]
-        sum += n
-    newsum = round(sum, 3)
-    f.close()
-    return newsum
+import json # Импорт библиотеки
+
+
+def task() -> float:  # Определяем функцию, которая возвращает тип float
+    f = "input.json"
+    with open(f) as a:  #Открываем файл json
+        data = json.load(a)  # Используем load для парсинга JSON данных из файла и сохраненяем их в переменную data
+    sm = sum([i["score"] * i["weight"] for i in data])   # Создаем список, где для каждого элемента `i` в данных data перемножаются score и weight
+    return round(sm, 3)  #  Округляем до трех знаков после запятой
+
+
 print(task())
 
